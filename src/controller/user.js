162bedbd9e373c4af9b7664d6d89a5d2990164a4
Norton Souza <norton.souza@ftdata.com.br>
2021-10-29 22:44:40 -0300

    Alterando para classe rotas de usuario

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,78 +1,83 @@
 const express = require('express');
 const User = require('../model/user');
-const router = express.Router();
 const bcryptjs = require('bcryptjs');
-const Authorization = require('../services/verifyToken');
 
-router.post('/user/register', async (req, res) => {
-    const { email, password, name } = req.body;
+class Users {
 
-    if (!(email && password && name)) {
-        return res.status(400).send("All input is required");
+    constructor(app) {
+        this._app = app;
     }
 
-    await User.findOne({ email }, (err, user) => {
-        if (user) return res.status(409).send("User Already Exist.");
-    });
+    static async Register(req, res) {
+        const { email, password, name } = req.body;
 
-    const encryptedPassword = await bcryptjs.hash(password, 10);
+        if (!(email && password && name)) {
+            return res.status(400).send("All input is required");
+        }
 
-    await User.create({
-        name,
-        email,
-        password: encryptedPassword,
-    });
+        await User.findOne({ email }, (err, user) => {
+            if (user) return res.status(409).send("User Already Exist.");
+        });
 
-    res.status(201).json('user created');
-});
+        const encryptedPassword = await bcryptjs.hash(password, 10);
 
-router.get('/user/list', Authorization, (req, res) => {
-    User.find({}, (err, users) => {
-        if (err) throw err;
-        if (users.length == 0) return res.status(404).send('Not Found');
-        return res.status(200).json(users);
-    });
-});
+        await User.create({
+            name,
+            email,
+            password: encryptedPassword,
+        });
 
-router.get('/user/:id', Authorization, (req, res) => {
-    User.findById(req.params.id, (err, user) => {
-        if (err || !user) return res.status(404).send('Not Found');
-        return res.status(200).json(user);
-    })
-});
-
-router.delete('/user/:id', Authorization, (req, res) => {
-    try {
-        User.deleteOne({ _id: req.params.id })
-            .then(resp => { if (resp.ok) return res.status(200).send('User deleted successfully') })
-            .catch(err => { return res.send(err) })
+        res.status(201).json('user created');
     }
-    catch (err) {
-        return res.status(500).send({ error: { status: 500, description: 'Operation error' } });
-    }
-});
 
-router.put('/user/update/:id', Authorization, (req, res) => {
-    if (!req.body.name || !req.body.email || !req.body.password)
-        res.status(400).send({ error: { status: 'Param invalid' } });
-    else {
-        User.findOneAndUpdate({ _id: req.params.id }, req.body, (err, data) => {
-            if (err) res.send(err);
-            data.modifyAt = new Date();
-            res.send(data);
+    static async List(req, res) {
+        User.find({}, (err, users) => {
+            if (err) throw err;
+            if (users.length == 0) return res.status(404).send({ error: { description: 'User Not Found' } });
+            return res.status(200).json(users);
         });
     }
-});
 
-router.patch('/user/update/password/:id', Authorization, (req, res) => {
-    if (!req.body.password)
-        res.status(400).send({ error: { status: 'Param invalid' } });
-    else {
-        User.findOneAndUpdate({ _id: req.params.id }, req.body.password, (err, data) => {
-            if (err) res.send(err);
-            res.send(data);
+    static async ListUser(req, res) {
+        User.findById(req.params.id, (err, user) => {
+            if (err || !user) return res.status(404).send({ error: { description: 'User Not Found' } });
+            return res.status(200).json(user);
         });
     }
-})
 
-module.exports = app => app.use('/api/v1', router);
+    static DeleteUser(req, res) {
+        try {
+            User.deleteOne({ _id: req.params.id })
+                .then(resp => { if (resp.ok) return res.status(200).send('User deleted successfully') })
+                .catch(err => { return res.send(err) })
+        }
+        catch (err) {
+            return res.status(404).send({ error: { description: 'User Not Found' } });
+        }
+    }
+
+    static UpdateUser(req, res) {
+        if (!req.body.name || !req.body.email || !req.body.password)
+            res.status(400).send({ error: { status: 'Param invalid' } });
+        else {
+            User.findOneAndUpdate({ _id: req.params.id }, req.body, (err, data) => {
+                if (err) res.send(err);
+                data.modifyAt = new Date();
+                res.send(data);
+            });
+        }
+    }
+
+    static UpdatePassword(req, res) {
+        if (!req.body.password)
+            res.status(400).send({ error: { status: 'Param invalid' } });
+        else {
+            User.findOneAndUpdate({ _id: req.params.id }, req.body.password, (err, data) => {
+                if (err) res.send(err);
+                res.send(data);
+            });
+        }
+    }
+}
+
+module.exports = Users;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const Auth = require('../controller/auth');
+const Users = require('../controller/user');
+const Authorization = require('../services/verifyToken');
 
 router
     .post('/auth', Auth.Login)
+    .post('/user/register', Users.Register)
+    .get('/user/list', Authorization, Users.List)
+    .get('/user/:id', Authorization, Users.ListUser)
+    .delete('/user/:id', Authorization, Users.DeleteUser)
+    .put('/user/update/:id', Authorization, Users.UpdateUser)
+    .patch('/user/update/password/:id', Authorization, Users.UpdatePassword)
 
 module.exports = router;
