c52428a9d24eee942fb723f56658036c1fbe2cc3
Norton Souza <norton.souza@ftdata.com.br>
2021-11-17 20:28:45 -0300

    Adicionando rota de deletar usuario

--- src/controller/user.js
+++ src/controller/user.js
@@ -21,8 +21,8 @@ class Users {
             const isUser = `SELECT cpf,email FROM lanchonete.user WHERE cpf='${_cpf}' AND email='${_email}';`;
             MySQL.query(isUser, (err, result) => {
                 if (err) return res.status(500).send({ err });
-                if (result.length > 0) {
-                    return res.status(409).send({ error: { description: "User Already Exist" } });
+                if (result.length) {
+                    return res.status(409).send({ error: { description: "Email or cpf Already Exist" } });
                 }
                 resolve();
             });
@@ -36,7 +36,7 @@ class Users {
             .then(() => {
                 const registerUser = `INSERT INTO lanchonete.user (cpf, full_name, email, password, phone_number) VALUES ('${_cpf}', '${full_name}', '${_email}', '${_password}', '${phone_number}');`;
 
-                MySQL.query(registerUser, (err, results) => {
+                MySQL.query(registerUser, (err, result) => {
                     if (err) return res.status(400).send({ err });
                     return res.status(201).send({ success: { description: 'User created successfully' } });
                 });
@@ -44,30 +44,37 @@ class Users {
             .catch(err => res.send({ err }))
     }
 
-    static async List(req, res) {
-        User.find({}, (err, users) => {
-            if (err) throw err;
-            if (users.length == 0) return res.status(404).send({ error: { description: 'User Not Found' } });
-            return res.status(200).json(users);
+    static List(req, res) {
+        const listUsers = "SELECT * FROM lanchonete.user";
+        MySQL.query(listUsers, (err, results) => {
+            if (err) return res.status(400).send({ err });
+            if (results.length) {
+                const users = results.map(item => {
+                    delete item.cpf;
+                    delete item.password;
+                    return { ...item, email: Crypt.descrypt(item.email) }
+                });
+                return res.status(200).send(users);
+            }
+            else return res.status(404).send({ error: { description: 'Not Found' } })
         });
     }
 
-    static async ListUser(req, res) {
-        User.findById(req.params.id, (err, user) => {
-            if (err || !user) return res.status(404).send({ error: { description: 'User Not Found' } });
-            return res.status(200).json(user);
+    static ListUser(req, res) {
+        if (!req.params.id) return res.status(400).send({ error: { description: 'ID invalid' } });
+        const listUsers = `SELECT * FROM lanchonete.user WHERE id_user='${req.params.id}'`;
+        MySQL.query(listUsers, (err, results) => {
+            if (err) return res.status(400).send({ err });
+            if (results.length) {
+                const user = results.map(item => {
+                    delete item.cpf;
+                    delete item.password;
+                    return { ...item, email: Crypt.descrypt(item.email) }
+                });
+                return res.status(200).send(user);
+            }
+            else return res.status(404).send({ error: { description: 'Not Found' } });
         });
-    }
-
-    static DeleteUser(req, res) {
-        try {
-            User.deleteOne({ _id: req.params.id })
-                .then(resp => { if (resp.ok) return res.status(200).send('User deleted successfully') })
-                .catch(err => { return res.send(err) })
-        }
-        catch (err) {
-            return res.status(404).send({ error: { description: 'User Not Found' } });
-        }
     }
 
     static UpdateUser(req, res) {
@@ -91,6 +98,26 @@ class Users {
                 res.send(data);
             });
         }
+    }
+
+    static DeleteUser(req, res) {
+        if (!req.params.id) return res.status(400).send({ error: { description: 'ID invalid' } });
+        let email = '';
+        const user = `SELECT email FROM lanchonete.user WHERE id_user='${req.params.id}'`;
+        const deleteUser = `DELETE FROM  lanchonete.user WHERE id_user='${req.params.id}'`;
+
+        MySQL.query(user, (err, results) => results.map(item => email = item.email));
+
+        MySQL.query(deleteUser, (err, result) => {
+            if (err) return res.status(400).send({ err });
+            if (result.affectedRows) {
+                const delelteLogin = `DELETE FROM  lanchonete.login WHERE email='${email}'`;
+                MySQL.query(delelteLogin, (err, result) =>
+                    res.status(201).send({ success: { description: 'User deleted successfully' } })
+                );
+    }
+            else return res.status(404).send({ error: { description: 'Not Found' } });
+        });
     }
 }
 
